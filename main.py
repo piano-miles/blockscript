8f93d42fea98b6ca130b11d12d507d7c44a6d9a5
Miles Waugh <miles.m.waugh@gmail.com>
2023-05-16 00:27:40 -0700

    Began work on sb3 --> bs3

--- main.py
+++ main.py
@@ -1,29 +1,88 @@
 # Read in project.json, parse it, and store it in a variable called project.
 
 import json
+import zipfile
 
 
-def jprint(obj):  # type: ignore
+def jprint(obj):
     print(json.dumps(obj, indent=4, sort_keys=True))
 
 
 def jprint(title, obj):
-    print(title+'\n')
+    print(title + "\n")
     print(json.dumps(obj, indent=4, sort_keys=True))
+
+
+def keyFromIndex(dict, n=0):
+    if n < 0:
+        n += len(dict)
+    for A, B in enumerate(dict.keys()):
+        if A == n:
+            return B
+    raise IndexError("dict index out of range")
+
+
+def rep(str, A, B):
+    return str.strip().replace(A, B)
 
 
 project_name = "project"
 
 # Read in project.json
-with open(project_name + '/project.json') as json_file:
+with open(project_name + "/project.json") as json_file:
     project = json.load(json_file)
 
 project_targets = project["targets"]
 stage = next((target for target in project_targets if target["isStage"]), None)
-project_targets = [
-    target for target in project_targets if not target["isStage"]]
+sprites = [target for target in project_targets if not target["isStage"]]
 
 if stage is not None:
     variables = stage["variables"]
     lists = stage["lists"]
     sounds = stage["sounds"]
+
+# sprites.blocks.index
+
+script = open("main.bs3", "w")
+header = """Stage {
+    // Global parameters and Stage properties go here
+
+}"""
+tab = "    "
+
+try:
+    # Stage
+
+    script.write("Stage {")
+    script.write(f"\n{tab}// Global parameters and Stage properties go here")
+
+    variables = stage["variables"]
+
+    for i in range(len(variables)):
+        variable = variables[keyFromIndex(variables, i)]
+        variable[0] = rep(variable[0], " ", "_")
+
+    for i in range(len(variables)):
+        variable = variables[keyFromIndex(variables, i)]
+        script.write(f"\n{tab}{variable[0]} = {variable[1]}")
+
+    script.write("\n}")
+
+    # Sprites
+
+    script.write("\n\n// Sprites")
+    for sprite in sprites:
+        blocks = sprite["blocks"]
+
+        script.write("\n\n")
+        script.write(sprite["name"] + " {\n")
+
+        # Blocks
+
+        for i in range(len(blocks)):
+            block = blocks[keyFromIndex(blocks, i)]
+            script.write(tab + block["opcode"].split("_")[1] + "\n")
+
+        script.write("}")
+finally:
+    script.close()
